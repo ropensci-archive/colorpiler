a0ced2f2d6d9121c2a9c4bf8f770710fd2b3e5b6
Mika Braginsky <mikabr@stanford.edu>
2016-03-31 15:06:38 -0700

    farben_palette gets json from github

--- R/farben.R
+++ R/farben.R
@@ -1,34 +1,35 @@
-#' solarized colour palette
+#' farben_palette
 #'
-#' Provides a colour scheme based on the
-#' \href{http://ethanschoonover.com/solarized}{solarized} accent colours.
-#'
-#' @param num_values Number of values to colour. If more than 9, colours start
-#'   to repeat.
+#' @param palette_name Name of palette.
 #'
 #' @examples
 #' if (requireNamespace("scales", quietly = TRUE)) {
 #'   library(scales)
-#'   show_col(farben_palette("example"))
+#'   show_col(farben_palette("wes_a_fave")(9))
 #' }
 #' @export
 farben_palette <- function(palette_name) {
   function(num_values) {
-    palette_data <- jsonlite::fromJSON(sprintf("../inst/%s.json", palette_name))
+    palette_url <- sprintf("https://raw.githubusercontent.com/vsbuffalo/farbenfroh/master/%s.json",
+                           palette_name)
+    palette_data <- jsonlite::fromJSON(RCurl::getURL(palette_url))
     palette_data$colors[1:num_values]
   }
 }
 
 #' farben colour palettes for ggplot2
 #'
+#' @param name Name of palette.
 #' @param ... Arguments passed on to discrete_scale to control name, limits,
 #'   breaks, labels and so forth.
 #'
 #' @examples
-#' library(ggplot2)
-#' ggplot(mtcars, aes(x = mpg, y = hp, colour = factor(cyl))) +
-#'   geom_point() +
-#'   scale_colour_farben("example")
+#' if (requireNamespace("ggplot2", quietly = TRUE)) {
+#'   library(ggplot2)
+#'   ggplot(mtcars, aes(x = mpg, y = hp, colour = factor(cyl))) +
+#'     geom_point() +
+#'     scale_colour_farben("wes_a_fave")
+#' }
 #' @export
 scale_colour_farben <- function(name, ...) {
   ggplot2::discrete_scale(aesthetics = "colour", scale_name = "farben",
