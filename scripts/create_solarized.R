4acc2190873d721c696621c12960bb95966bce09
Mika Braginsky <mikabr@stanford.edu>
2016-04-01 13:17:08 -0700

    script for creating solarized palette

---
+++ scripts/create_solarized.R
@@ -0,0 +1,16 @@
+solarized_colors <- c(magenta = "#d33682",
+                      red = "#dc322f",
+                      orange = "#cb4b16",
+                      yellow = "#b58900",
+                      green = "#859900",
+                      cyan = "#2aa198",
+                      blue = "#268bd2",
+                      violet = "#6c71c4")
+
+create_palette(name = "solarized",
+               authors = "Ethan Schoonover",
+               github_user = "altercation",
+               description = "Precision colors for machines and people",
+               keywords = "",
+               colors = solarized_colors,
+               type = "qualitative")
